c07546c0e81aa78e3fe12da9414e473d56adba98
Yuto <yutonakagame@gmail.com>
2024-03-11 19:54:33 +0900

    [UI/Common/Footer] - Created Footer - Created Footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,31 @@
+<nav class="navbar sticky-bottom style-seet color1" style="background-color: #FF3A3A;">
+    <div class="container-fluid">
+        <ul class="navbar-nav ms-auto list-group-horizontal align-items-center">
+            <li class="nav-item me-4">
+                <a class="nav-link" href="">
+                    <i class="fa-brands fa-x-twitter navbar-icon" style="font-size: 1.5rem; color: white;"></i>
+                </a>
+            </li>
+            <li class="nav-item me-4">
+                <a class="nav-link" href="">
+                    <i class="fa-brands fa-facebook navbar-icon" style="font-size: 1.5rem; color: white;"></i>
+                </a>
+            </li>
+            <li class="nav-item me-4">
+                <a class="nav-link" href="">
+                    <i class="fa-brands fa-instagram navbar-icon" style="font-size: 1.5rem; color: white;"></i>
+                </a>
+            </li>
+            <li class="nav-item me-4">
+                <a class="nav-link" href="">
+                    <h4 class="text-white mb-0">About Us</h4>
+                </a>
+            </li>
+            <li class="nav-item me-4">
+                <a class="nav-link" href="">
+                    <h4 class="text-white mb-0">FAQ</h4>
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>

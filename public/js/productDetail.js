51c07911e164a51989d2e47af844483994f4660e
Yuto <yutonakagame@gmail.com>
2024-03-15 21:32:06 +0900

    [UI/Customer/ProductDetail] - Fixed productDetail.js - remove var incrementPlus = - remove var incrementMinus =

--- public/js/productDetail.js
+++ public/js/productDetail.js
@@ -1,12 +1,12 @@
 var buttonPlus  = $(".qty-btn-plus");
 var buttonMinus = $(".qty-btn-minus");
 
-var incrementPlus = buttonPlus.click(function() {
+buttonPlus.click(function() {
     var $n = $(this).parent(".qty-container").find(".input-qty");
     $n.val(Number($n.val())+1 );
 });
 
-var incrementMinus = buttonMinus.click(function() {
+buttonMinus.click(function() {
     var $n = $(this).parent(".qty-container").find(".input-qty");
 
     var amount = Number($n.val());

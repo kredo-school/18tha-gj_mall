afe67db08c9701fc27ba3bb22b70c452f4230f7c
Shun <hanamotoshun1@gmail.com>
2024-03-16 18:04:26 +0900

    [UI/Admin/AdminDashboard/ManagementUser] - Created blade-page and css-page

---
+++ resources/views/admin/management/modal/delete.blade.php
@@ -0,0 +1,29 @@
+{{-- Delete User --}}
+<link rel="stylesheet" href="{{ asset('css/admin/management/deleteUser.css') }}">
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+<div class="modal fade " id="delete-user-">
+  <div class="modal-dialog">
+      <div class="modal-content">
+          <div class="modal-header flex-column delete-header">
+              <h1 class="modal-title fw-bold">Delete Admin ID - 1</h1>
+              <p class="mb-0 small-title">Are you sure you want to delete this user?</p>
+          </div>
+
+        <div class="modal-body">
+            <p><strong class="fw-bold"> Name: </strong> John Smith</p>
+            <p><strong class="fw-bold">E-mail: </strong>johnsmith@gmail.com</p>
+            <p><strong class="fw-bold">Role: </strong>Admin</p>                
+        </div>
+        <div class="modal-footer border-0 mx-auto montserrat">
+            <form action="#" method="#">
+                @csrf
+                {{-- @method('') --}}
+                <button type="button" class="btn btn-sm custom-button4 shadow me-1" data-bs-dismiss="modal">
+                    Cancel
+                </button>
+                <button type="submit" class="btn btn-sm custom-button5 shadow ms-1">Delete User</button>
+            </form>
+        </div>
+  </div>
+</div>

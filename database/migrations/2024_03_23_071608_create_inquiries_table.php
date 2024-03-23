b68e570f3d8162098e5a011276401e9a677cafb7
Chie0430 <chii.r.h@gmail.com>
2024-03-23 16:58:32 +0900

    [Table/Create/240323] - Create DB tables.(only structures)

---
+++ database/migrations/2024_03_23_071608_create_inquiries_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inquiries', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->text('answer')->nullable();
+            $table->text('translated_answer')->nullable();
+            $table->unsignedBigInteger('customer_id');
+            $table->unsignedBigInteger('product_id')->nullable();
+            $table->unsignedBigInteger('genre_id');
+            $table->unsignedBigInteger('inquiry_status_id')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inquiries');
+    }
+};

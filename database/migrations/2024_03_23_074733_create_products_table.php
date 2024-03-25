27ba45770050ee0edffaa901025a0e3f41731af3
Shun <hanamotoshun1@gmail.com>
2024-03-25 22:21:08 +0900

    [Table/Create/240323Part2] - Create Table

---
+++ database/migrations/2024_03_23_074733_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('price');
+            $table->integer('qty_in_stock');
+            $table->text('description');
+            $table->unsignedBigInteger('status_id');
+            $table->unsignedBigInteger('seller_id');
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('product_detail_id');
+            $table->timestamps();
+
+            // $table->foreign('status_id')->references('id')->on('products_status');
+            // $table->foreign('seller_id')->references('id')->on('sellers');
+            // $table->foreign('category_id')->references('id')->on('categories');
+            // $table->foreign('product_detail_id')->references('id')->on('product_details');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};

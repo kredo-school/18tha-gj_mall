6747d7b088bd8cc7d0e5f44d71bbb6db7aa43afb
Chie0430 <chii.r.h@gmail.com>
2024-03-26 22:23:03 +0900

    Create table seeders.

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategorySeeder extends Seeder
+{
+    private $category;
+
+    public function __construct(Category $category)
+    {
+        $this->category = $category;
+    }
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+            [
+                'name' => 'Food';
+            ]
+            [
+                'name' => 'Cloth/Accessory';
+            ],
+            [
+                'name' => 'Kitchen';
+            ],
+            [
+                'name' => 'Home Decor';
+            ],
+            [
+                'name' => 'Table';
+            ]
+        ];
+
+        $this->category->insert($categories); // insert the values to categories table
+    }
+}

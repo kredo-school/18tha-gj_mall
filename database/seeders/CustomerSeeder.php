22904a321fdd54e2a1b02b2958126505f370ddd0
Kaori Suzuki <kbundo77@gmail.com>
2024-04-01 14:29:38 +0300

    [Table/Create/Seeders_3] -add Customer Seeder & Ad Seeder

--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Customer;
+use App\Models\Users\Customer;
 
 class CustomerSeeder extends Seeder
 {

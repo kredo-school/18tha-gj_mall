1c898d2b1153cb051f2acb51e89d7a9d76cd90f5
Yuto <yutonakagame@gmail.com>
2024-04-02 20:38:39 +0800

    [Backend/All/Login] - Delete unnecessary code - AdminLoginController - delete __construct - SellerLoginController - delete __construct

--- app/Http/Controllers/Users/AdminController.php
+++ app/Http/Controllers/Users/AdminController.php
@@ -8,13 +8,6 @@ use App\Models\Users\Admin;
 
 class AdminController extends Controller
 {
-    private $admin;
-
-    public function __construct(Admin $admin)
-    {
-        $this->admin = $admin;
-    }
-
     public function showDashboard()
     {
         return view('admin.dashboard');

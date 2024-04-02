1cfb0d533caebb7f3543b51c17c020d62626f879
Chie0430 <chii.r.h@gmail.com>
2024-04-02 21:17:53 +0900

    [Backend/Inquiry] - Create inquiry page functions. 1. Create Model - Inquiries/Inquiry, Inquiries/InquiryGenre 2. Create Controller - InquiryController. 3. Modified inquiry view - Added back-end function.

---
+++ app/Models/Inquiries/Inquiry.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\Inquiries;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Inquiry extends Model
+{
+    use HasFactory;
+
+    protected $inquiry = 'inquiries';
+
+}
---
+++ app/Models/Inquiries/InquiryGenre.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Inquiries;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class InquiryGenre extends Model
+{
+    use HasFactory;
+
+    protected $inquiry_genre = 'inquiry_genres';
+}

45e3659f8ab567824a2a0e728e13cb01b78bc42c
Yuto <yutonakagame@gmail.com>
2024-04-03 09:01:11 +0800

    [Backend/Customer/Profile] - temporary save

--- app/Http/Controllers/Users/CustomerController.php
+++ app/Http/Controllers/Users/CustomerController.php
@@ -24,5 +24,29 @@ class CustomerController extends Controller
     public function editProfile(Request $request, $id)
     {
         $customer = $this->customer->findOrFail($id);
+        $request->validate([
+            // Profile
+            'fname'        => 'required|max:255',
+            'lname'        => 'required|max:255',
+            'email'        => 'required|email|unique:customers,email,'. $id,
+            'phone_number' => 'unique:customers,email,'. $id,
+            'new_password' => 'required|min:8|max:255',
+
+            // Address
+            'unit-num'  => 'max:255',
+            'st-num'    => 'max:255',
+            'address1'  => 'required|max:255',
+            'address2'  => 'max:255',
+            'city'      => 'required|max:255',
+            'region'    => 'required|max:255',
+            'pos-code'  => 'required|max:12',
+            'country'   => 'required|integer',
+            
+            // Payment
+            'name'        => 'max:255',
+            'card_number' => 'required|int|',
+            'expire_date' => 'required|max:255',
+            'cvv'         => 'required|max:255',
+        ]);
     }
 }

d940c92b9840654ded76b7fc13576fab1ef1d09e
Kaori Suzuki <kbundo77@gmail.com>
2024-04-20 05:33:29 +0300

    [Backend/Option/Pageview] -fix logic of LogPageview

--- app/Http/Middleware/LogPageViews.php
+++ app/Http/Middleware/LogPageViews.php
@@ -7,20 +7,39 @@ use App\Models\PageView;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Session;
 
 class LogPageViews
 {
     public function handle(Request $request, Closure $next)
     {
-        $customer = Auth::guard("customer"); // Get the authenticated user
 
-        // Log the page view
-        PageView::create([
+        // Get the last visited URL from the session
+        $lastVisitedUrl = Session::get('last_visited_url');
 
-            'customer_id' => $customer ? $customer->id() : null,
-            'url' => $request->url(),
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+        // Proceed with the request
+        $response = $next($request);
+
+        // Get the current URL
+        $currentUrl = $request->url();
+
+        if ($lastVisitedUrl !== $currentUrl) {
+
+            $customer = Auth::guard("customer"); // Get the authenticated user
+
+            // Log the page view
+            PageView::create([
+
+                'customer_id' => $customer ? $customer->id() : null,
+                'url' => $currentUrl ,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+
+        // Update the last visited URL in the session
+        Session::put('last_visited_url', $currentUrl);
+
+        return $response;
     }
 }

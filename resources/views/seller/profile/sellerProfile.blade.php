cf10d36491c1062977b1fadf3ea09c33b03da79a
Kaori Suzuki <kbundo77@gmail.com>
2024-04-24 15:33:09 +0300

    [Backend/Seller/Debugs] --fix sellerProfilePage layouts

--- resources/views/seller/profile/sellerProfile.blade.php
+++ resources/views/seller/profile/sellerProfile.blade.php
@@ -8,17 +8,18 @@
     <link rel="stylesheet" href="{{ asset('css/search.css') }}">
 
     @include('layouts.navbar')
-    
+
     <div class="container-fluid">
         <div class="row mb-5">
             <div class="col p-0">
                 {{-- Carousel --}}
-                @forelse ($sellerProducts as $product)                    
+                @forelse ($sellerProducts as $product)
                     <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
                         <div class="carousel-inner">
                             @foreach ($product->ads as $ad)
                                 <div class="carousel-item {{ $loop->first ? 'active' : '' }}" style="height: 450px;">
-                                    <img src="{{ asset('storage/images/ads/'. $ad->image_name) }}" class="d-block w-100 h-100" alt="Ad Image" style="object-fit: cover">
+                                    <img src="{{ asset('storage/images/ads/' . $ad->image_name) }}"
+                                        class="d-block w-100 h-100" alt="Ad Image" style="object-fit: cover">
                                     <div id="content" class="carousel-caption d-block text-white">
                                         <h4>{{ $ad->title }}</h4>
                                         <p class="lead text-truncate">{{ $ad->content }}</p>
@@ -26,7 +27,7 @@
                                 </div>
                             @endforeach
                         </div>
-                    </div>          
+                    </div>
                 @empty
                     <h1>No Advertisment yet.</h1>
                 @endforelse
@@ -42,9 +43,13 @@
                     <div class="col-6">
                         <div class="text-center">
                             @if ($sellerProfile->avatar)
-                                <img src="{{ asset('storage/images/customer/') }}" class="rounded img-fluid mx-auto d-block" alt="shopImage" style="width: 800px; height: 550px;">                          
+                                <img src="{{ asset('storage/images/sellers/' . $sellerProfile->avatar) }}"
+                                    class="rounded img-fluid mx-auto d-block" alt="shopImage"
+                                    style="width: 800px; height: 550px;">
                             @else
-                                <img src="{{ asset('images/seller/shopImg.svg') }}" class="rounded img-fluid mx-auto d-block" alt="shopImage" style="width: 800px; height: 550px;">                        
+                                <img src="{{ asset('images/seller/shopImg.svg') }}"
+                                    class="rounded img-fluid mx-auto d-block" alt="shopImage"
+                                    style="width: 800px; height: 550px;">
                             @endif
                         </div>
                     </div>
@@ -53,16 +58,19 @@
 
                         <div class="row mx-auto mb-3">
                             <div class="col">
-                                <h4>{{ $sellerProfile->last_name}}{{ $sellerProfile->first_name}}</h4>
-                                <p>{{ $sellerProfile->description}}</p>
+                                <h4>{{ $sellerProfile->last_name }} {{ $sellerProfile->first_name }}</h4>
+                                <p>{{ $sellerProfile->description }}</p>
                             </div>
                         </div>
-                        
+
                         <div class="row mx-auto mb-3">
                             <div class="col">
                                 <h4>Address</h4>
                                 <p>
-                                    {{ $sellerProfile?->address?->street_number }} {{ $sellerProfile?->address?->address_line1 }}, {{ $sellerProfile?->address?->city }}, {{ $sellerProfile?->address?->postal_code }}, {{ $sellerProfile?->address?->country->name }}
+                                    {{ $sellerProfile?->address?->street_number }}
+                                    {{ $sellerProfile?->address?->address_line1 }}, {{ $sellerProfile?->address?->city }},
+                                    {{ $sellerProfile?->address?->postal_code }},
+                                    {{ $sellerProfile?->address?->country->name }}
                                 </p>
                             </div>
                         </div>
@@ -88,51 +96,58 @@
                 <div class="p-3">
                     <h1 class="mb-3">Shop Products List</h1>
 
-                    <div class="row row-cols-xxl-auto row-cols-lg-auto row-cols-md-auto row-cols-xs-1 g-3 mb-5" id="card-row">
+                    <div class="row row-cols-xxl-auto row-cols-lg-auto row-cols-md-auto row-cols-xs-1 g-3 mb-5"
+                        id="card-row">
                         @forelse ($sellerProducts as $item)
                             <div class="col">
                                 <div class="card h-100" id="card-item">
                                     @if ($item->productImage->isNotEmpty())
-                                        <img src="{{ asset('storage/images/items/'. $item->productImage->first()->productImages->image) }}" class="card-img-top" alt="{{ $item->name }}">
+                                        <img src="{{ asset('storage/images/items/' . $item->productImage->first()->productImages->image) }}"
+                                            class="card-img-top" alt="{{ $item->name }}">
                                     @else
-                                        <img src="{{ asset('images/items/no-image.svg') }}" alt="Product Image" class="card-img-top">
+                                        <img src="{{ asset('images/items/no-image.svg') }}" alt="Product Image"
+                                            class="card-img-top">
                                     @endif
-        
+
                                     @auth
                                         @if ($item->isFavorite())
                                             <form action="{{ route('favorite.destroy', $item->id) }}" method="post">
                                                 @csrf
                                                 @method('DELETE')
-                            
+
                                                 <button type="submit" class="btn btn-sm shadow-none p-0">
-                                                    <i class="fa-solid fa-heart position-absolute top-0 end-0 m-3 heart-icon"></i>
+                                                    <i
+                                                        class="fa-solid fa-heart position-absolute top-0 end-0 m-3 heart-icon"></i>
                                                 </button>
                                             </form>
                                         @else
                                             <form action="{{ route('favorite.store', $item->id) }}" method="post">
                                                 @csrf
                                                 <button type="submit" class="btn btn-sm shadow-none p-0">
-                                                    <i class="fa-solid fa-heart position-absolute top-0 end-0 m-3 heart-icon-no-favorite"></i>
+                                                    <i
+                                                        class="fa-solid fa-heart position-absolute top-0 end-0 m-3 heart-icon-no-favorite"></i>
                                                 </button>
                                             </form>
                                         @endif
                                     @endauth
-        
+
                                     <div class="card-body">
                                         <div class="row">
                                             <div class="col">
-                                                <a href="{{ route('productDetail', $item->id) }}" class="text-decoration-none text-dark">
+                                                <a href="{{ route('productDetail', $item->id) }}"
+                                                    class="text-decoration-none text-dark">
                                                     <h3 class="text-truncate">{{ $item->name }}</h3>
                                                 </a>
                                             </div>
                                         </div>
 
                                         <h4>${{ $item->price }}</h4>
-                         
+
                                         <ul class="list-group list-group-horizontal align-items-center my-2">
                                             @for ($i = 0; $i < 5; $i++)
                                                 <li class="list-group-item p-0 border-0">
-                                                    <i class="fa-solid fa-star{{ $i < $item->averageRating ? ' text-warning' : '' }}"></i>
+                                                    <i
+                                                        class="fa-solid fa-star{{ $i < $item->averageRating ? ' text-warning' : '' }}"></i>
                                                 </li>
                                             @endfor
                                             <li class="list-group-item p-0 border-0">
@@ -141,7 +156,7 @@
                                         </ul>
                                     </div>
                                 </div>
-                            </div>   
+                            </div>
                         @empty
                             <img src="{{ asset('images/common/noResults.svg') }}" alt="No Results" class="center">
                         @endforelse
